83da01574dfe0686c2eea83d0182706bceb5dfa4
Kaiky <kaikyxcunha@gmail.com>
2026-01-21 03:33:33 -0300

    Criada nova tabela de imagens, agora a relação entre ficha e fotos é 1:N

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class Ficha(db.Model):
     __tablename__ = 'fichas'
 
     id = db.Column(db.Integer, primary_key=True)
-    numero_ficha = db.Column(db.String(50))
+    numero_ficha = db.Column(db.String(50), unique=True)
     avaliacao = db.Column(db.Integer)
     
     autor = db.Column(db.String(200))
@@ -28,6 +28,20 @@ class Ficha(db.Model):
     tratamento_volumes = db.Column(JSONB)
 
     observacoes = db.Column(db.Text)
-    foto_path = db.Column(db.String(255))
     tecnico_nome = db.Column(db.String(150))
     data_preenchimento = db.Column(db.Date)
+
+    imagens = db.relationship('Imagem', backref='ficha', cascade='all, delete-orphan')
+
+
+class Imagem(db.Model):
+    __tablename__ = 'imagens'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    
+    caminho = db.Column(db.String(255), nullable=False)
+    
+    ficha_id = db.Column(db.Integer, db.ForeignKey('fichas.id'), nullable=False)
+    
+    def __repr__(self):
+        return f'<Imagem {self.caminho}>'
